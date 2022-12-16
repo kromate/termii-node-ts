4124f5dbe9e0a8870f8d242be30b45d88b782dce
kromate <akpananthony33@gmail.com>
2022-12-16 23:44:01 +0100

    extending messaging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Base } from "./base";
 import { Sender } from "./sender";
+import { Messaging } from "./Messaging";
 import { applyMixins } from "./utils";
 
 class Termii extends Base {}
-interface Termii extends Sender {}
+interface Termii extends Sender, Messaging {}
 
-applyMixins(Termii, [Sender]);
+applyMixins(Termii, [Sender, Messaging]);
 
 export default Termii;
